bc3a4169b9269430d06179b0ff3b742453a1bdbf
ChubmcFlubby <117937033+ChubmcFlubby@users.noreply.github.com>
2022-12-20 12:51:35 -0500

    First Commit - Creation of My world and our hero.

--- MyWorld.java
+++ MyWorld.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MyWorld here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Matthew Chiu) 
+ * @version (1.0.0. - December 2022)
  */
 public class MyWorld extends World
 {
