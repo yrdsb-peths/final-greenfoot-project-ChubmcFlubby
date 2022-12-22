488b1afb061896ce0a1d1eae77a7c237e3906b24
ChubmcFlubby <117937033+ChubmcFlubby@users.noreply.github.com>
2022-12-22 13:28:32 -0500

    Added a hero to the game, allowed it to move in 2D

--- Hero.java
+++ Hero.java
@@ -8,12 +8,25 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Hero extends Actor
 {
+    int speed = 2;
+    String facing = "right";
     /**
      * Act - do whatever the Hero wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
     public void act()
     {
-        // Add your action code here.
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-speed);
+            facing = "left";
+        }
+        
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(speed);
+            facing = "right";
+        }
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    Hero hero = new Hero();
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -17,5 +17,9 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        addObject(hero, 300, 300);
     }
+    /**
+     * Counstructor - The code that gets run one time when object is created.
+     */
 }
