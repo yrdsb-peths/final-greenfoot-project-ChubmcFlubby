9c705e9570bee8339d1dafb65d3a318b78cba020
ChubmcFlubby <348619313@gapps.yrdsb.ca>
2023-01-09 13:09:02 -0500

    Restarting

--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-    Hero hero = new Hero();
+    
     /**
      * Constructor for objects of class MyWorld.
      * 
